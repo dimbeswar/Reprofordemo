8696e8b81b7ed243000a71a251f07e7a2aff2669
dimbeswar <dimbeswar.pathak@hotmail.com>
2025-10-13 13:08:50 +0530

    Again added

--- Conversion.java
+++ Conversion.java
@@ -2,23 +2,19 @@ import java.util.Arrays;
 
 public class Conversion {
 
-	public static void main(String[] args) {
-		
-		int[] intArray = {1, 2, 3, 4, 5};
-		char[] charArray = {'a', 'b', 'c', 'd', 'e'};
-		
-		String[] combined = new String[intArray.length];
-		
-		for (int i = 0; i < intArray.length; i++) {
-			combined[i] = intArray[i] + "" + charArray[i];
-		
-		}
-<<<<<<< HEAD
-		System.out.println("No changes"+Arrays.toString(combined));
-System.out.println("No changes");
+    public static void main(String[] args) {
 
-=======
-		System.out.println(Arrays.toString(combined));
->>>>>>> 033be22de0b9706d0098a27ca1acd3e04e4eccb0
-}
+        int[] intArray = {1, 2, 3, 4, 5};
+        char[] charArray = {'a', 'b', 'c', 'd', 'e'};
+
+        String[] combined = new String[intArray.length];
+
+        for (int i = 0; i < intArray.length; i++) {
+            combined[i] = intArray[i] + "" + charArray[i];
+
+        }
+        System.out.println("No changes"+Arrays.toString(combined));
+
+
+    }
 }
