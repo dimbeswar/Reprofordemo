22d486dc47acc8d45bda3299823b8fd3d922b3f5
dimbeswar <dimbeswar.pathak@hotmail.com>
2025-10-13 11:00:38 +0530

    First file

---
+++ Conversion.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = {1, 2, 3, 4, 5};
+		char[] charArray = {'a', 'b', 'c', 'd', 'e'};
+		
+		String[] combined = new String[intArray.length];
+		
+		for (int i = 0; i < intArray.length; i++) {
+			combined[i] = intArray[i] + "" + charArray[i];
+		
+		}
+		System.out.println("No changes"+Arrays.toString(combined));
+}
+}
